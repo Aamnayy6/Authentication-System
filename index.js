1462fdaab98919a0c1809e404b2d0b612a1218b2
aamna <aamnaozair16@gmail.com>
2023-10-06 01:39:34 +0300

    aligned code

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-import  express  from "express";
+import express from "express";
 import authRoutes from "./Routes/authRoutes.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(express.json());
-mongoose.connect(process.env.mongoDB_URI, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-.then(()=>{
+mongoose
+  .connect(process.env.mongoDB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("connected to db");
-}).catch(err=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 app.use("/auth", authRoutes);
-app.listen(3000, ()=>{
-    console.log("Listening on port 3000");
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
--- middleware/registerMiddleware.js
+++ middleware/registerMiddleware.js
@@ -1,15 +1,15 @@
 import express from "express";
 import User from "../models/userModel.js";
-export const checkUniqueEmail= async(req, res, next)=>{
-    try{
-    const {email} = req.body;
-    const userExists = await User.findOne({email:email});
-    if(userExists){
-        res.status(400).send("User already exists");
-        return;
+export const checkUniqueEmail = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const userExists = await User.findOne({ email: email });
+    if (userExists) {
+      res.status(400).send("User already exists");
+      return;
     }
     next();
-}catch(err){
+  } catch (err) {
     res.status(500).send(err);
-}
+  }
 };
