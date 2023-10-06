042e0ddc35d263e3a60e07489f35dae8d95f1940
aamna <aamnaozair16@gmail.com>
2023-10-06 16:44:03 +0300

    added password hashing

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,14 +1,15 @@
 import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
 export const registerUser= async (req, res)=>{
         try{
         const {email, userName, password} = req.body;
         const user = new User({
             email:email,
             userName:userName,
-            password:password
+            password:bcrypt.hashSync(password, 10),
         });
         await user.save();
-        res.send("added");
+        res.send(user);
         }catch(err){
             res.status(500).send(err);
         }
