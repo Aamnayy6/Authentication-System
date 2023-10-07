df7a32088d6bad1726337c3cc5e65241cc5930e5
aamna <aamnaozair16@gmail.com>
2023-10-07 21:39:27 +0300

    added schema mehtod to compare pwd

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema({
   name: String,
   password: String,
   email: String,
 });
+userSchema.methods.comparePassword = async function(userPassword){
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  if(isMatch){
+    return true;
+  }
+  return false;
+}
 const User = new mongoose.model("Visitor", userSchema);
 export default User;
