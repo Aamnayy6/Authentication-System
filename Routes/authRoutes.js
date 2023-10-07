87f153ce0ba503d28e0ff67d2d5c53c16efe50ce
aamna <aamnaozair16@gmail.com>
2023-10-07 21:39:48 +0300

    added login route

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -3,4 +3,5 @@ import * as authHandlers from "../Controllers/authController.js";
 import * as authMiddleware from "../middleware/registerMiddleware.js";
 const router = express.Router();
 router.post("/register", authMiddleware.checkUniqueEmail,authHandlers.registerUser);
+router.get("/login", authMiddleware.checkEmailExists,authHandlers.loginUser);
 export default router;
